ff5e9a987428fb4deaae46a658234c10293d6087
tlc2880 <tommylcao@gmail.com>
2023-11-29 04:44:10 -0800

    Add style, fonts, and images

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import UsersLogo from './user-group.png'
 
 export default function Navbar() {
   return (
     <nav>
-      <h1>Users Helpdesk</h1>
+      <Image
+        src={UsersLogo}
+        alt='Users Typicode logo'
+        width={70}
+        placeholder='blur'
+        quality={100}
+      />
+      <h1>Users Typicode</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/users">Users</Link>
     </nav>
