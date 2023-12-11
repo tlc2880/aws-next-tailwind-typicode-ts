2ee73bcece5376fdfb0e83da5badb059a1dfcede
tlc2880 <tommylcao@gmail.com>
2023-12-11 07:44:41 -0800

    Add globals.css to root page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import './globals.css'
 import { Inter } from '@next/font/google'
 import Link from "next/link"
-import './globals.css'
+/*  
+Change
+*/
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
+    <main >
       <h2>Dashboard</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero repellendus tempore, exercitationem odit, quasi doloremque possimus recusandae alias sequi totam soluta natus iure eius, obcaecati sint dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed voluptates iste cum totam, facere explicabo, fugit suscipit ratione aspernatur consequuntur ex mollitia quaerat?</p>
 
