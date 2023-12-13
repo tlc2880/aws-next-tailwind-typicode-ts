b5191ed7cf7f360d364b2177e0eeb3b63b409e7c
tlc2880 <tommylcao@gmail.com>
2023-12-13 05:01:38 -0800

    Add dynamic routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import './globals.css'
 import { Inter } from '@next/font/google'
 import Link from "next/link"
-/*  
-Change
-*/
 
 const inter = Inter({ subsets: ['latin'] })
 
--- app/users/UsersList.tsx
+++ app/users/UsersList.tsx
@@ -1,30 +1,32 @@
-import '../globals.css'
+import { UserType } from '../types.d'
 import type { Metadata } from 'next'
 import getAllUsers from '@/lib/getAllUsers'
 import Link from "next/link"
-import { UserType } from '../types.d'
 
 export const metadata: Metadata = {
   title: 'Users',
 }
 
-export default async function UserList() {
+export default async function UsersList() {
   const usersData: Promise<UserType[]> = getAllUsers()
-
   const users = await usersData
-  return (
-    <>
-      {users.map(( user: UserType ) => (
-        <div key={user.id} className="card my-5">
-          <h3>{user.name}</h3>
-          <p>UserName: {user.username}</p>
-          <p>Email: {user.email}</p>
-          <p>Company: {user.company.name}</p>
-        </div>
-      ))}
-      {users.length === 0 && (
-        <p className="text-center">There are no open users, yay!</p>
-      )}
-    </>
+
+  const content = (
+    <section>
+      <>
+        {users.map(( user: UserType ) => (
+          <div key={user.id} className="card my-5">
+            <h3><Link href={`/users/${user.id}`}>{user.name}</Link></h3>
+            <p>UserName: {user.username}</p>
+            <p>Email: {user.email}</p>
+            <p>Company: {user.company.name}</p>
+          </div>
+        ))}
+        {users.length === 0 && (
+          <p className="text-center">There are no open users, yay!</p>
+        )}
+      </>
+    </section>
   )
+  return content
 }
